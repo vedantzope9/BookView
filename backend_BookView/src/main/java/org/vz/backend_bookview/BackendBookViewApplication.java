a43f6f11214231aa754c61f2a10ab5c3863a0094
Vedant Zope <vedantzope9@gmail.com>
2025-01-26 10:52:12 +0530

    Backend setup

---
+++ backend_BookView/src/main/java/org/vz/backend_bookview/BackendBookViewApplication.java
@@ -0,0 +1,13 @@
+package org.vz.backend_bookview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBookViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBookViewApplication.class, args);
+	}
+
+}
---
+++ backend_BookView/src/test/java/org/vz/backend_bookview/BackendBookViewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vz.backend_bookview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBookViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
